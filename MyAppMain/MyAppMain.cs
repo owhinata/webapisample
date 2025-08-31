80094b5030da2935570830a9e0d27e55de3d23d6
KatsumiOuwa <d2p.yggdrasill@gmail.com>
2025-08-31 20:36:18 +0900

    refactor: subscribe to events in MyAppMain constructor to avoid post-start race

--- MyAppMain/MyAppMain.cs
+++ MyAppMain/MyAppMain.cs
@@ -2,31 +2,33 @@ using MyWebApi;
 
 namespace MyAppMain;
 
-public sealed class MyAppMain
+public sealed class MyAppMain : IDisposable
 {
     private readonly Func<string, Task> _onStart;
     private readonly Func<string, Task> _onEnd;
     private readonly MyWebApiHost _host = new();
+    private bool _disposed;
 
     public MyAppMain(Func<string, Task> onStart, Func<string, Task> onEnd)
     {
         _onStart = onStart ?? (_ => Task.CompletedTask);
         _onEnd = onEnd ?? (_ => Task.CompletedTask);
+        // Subscribe at construction time to avoid missing early POSTs right after Start.
+        _host.StartRequested += _onStart;
+        _host.EndRequested += _onEnd;
     }
 
     public bool IsRunning => _host.IsRunning;
 
-    public void Start(int port)
-    {
-        _host.StartRequested += _onStart;
-        _host.EndRequested += _onEnd;
-        _host.Start(port);
-    }
+    public void Start(int port) => _host.Start(port);
 
-    public void Stop()
+    public void Stop() => _host.Stop();
+
+    public void Dispose()
     {
-        _host.Stop();
+        if (_disposed) return;
         _host.StartRequested -= _onStart;
         _host.EndRequested -= _onEnd;
+        _disposed = true;
     }
 }
