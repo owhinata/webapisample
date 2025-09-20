92b7a8aecf903c0dc62182b45ffecc891db494f0
KatsumiOuwa <d2p.yggdrasill@gmail.com>
2025-09-20 17:43:08 +0900

    test: handle multiple imu state notifications

--- MyAppMain.Tests/MyAppMainBlackBoxTests.cs
+++ MyAppMain.Tests/MyAppMainBlackBoxTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -199,12 +200,17 @@ public class MyAppMainBlackBoxTests
         var connected = new TaskCompletionSource<bool>(
             TaskCreationOptions.RunContinuationsAsynchronously
         );
+        var imuOnObserved = false;
         var imuOn = new TaskCompletionSource<bool>(
             TaskCreationOptions.RunContinuationsAsynchronously
         );
         var imuSample = new TaskCompletionSource<bool>(
             TaskCreationOptions.RunContinuationsAsynchronously
         );
+        var sampleBeforeOn = new TaskCompletionSource<bool>(
+            TaskCreationOptions.RunContinuationsAsynchronously
+        );
+        var stateNotifications = new List<bool>();
         hub2.StartCompleted += res =>
         {
             if (res.Success)
@@ -213,10 +219,27 @@ public class MyAppMainBlackBoxTests
         hub2.ImuConnected += _ => connected.TrySetResult(true);
         hub2.ImuStateUpdated += dto =>
         {
+            lock (stateNotifications)
+            {
+                stateNotifications.Add(dto.IsOn);
+            }
+
             if (dto.IsOn)
+            {
+                imuOnObserved = true;
                 imuOn.TrySetResult(true);
+            }
         };
-        hub2.ImuSampleReceived += _ => imuSample.TrySetResult(true);
+        hub2.ImuSampleReceived += _ =>
+        {
+            if (!imuOnObserved)
+            {
+                sampleBeforeOn.TrySetResult(true);
+                return;
+            }
+
+            imuSample.TrySetResult(true);
+        };
         var app = new global::MyAppMain.MyAppMain(hub2);
         var apiPort = GetFreeTcpPort();
 
@@ -239,8 +262,41 @@ public class MyAppMainBlackBoxTests
             // Wait for IMU flow: start -> connected -> ON -> sample
             await WaitAsync(startDone.Task, TimeSpan.FromSeconds(3));
             await WaitAsync(connected.Task, TimeSpan.FromSeconds(3));
-            await WaitAsync(imuOn.Task, TimeSpan.FromSeconds(3));
-            await WaitAsync(imuSample.Task, TimeSpan.FromSeconds(3));
+            var onOrFailure = await Task.WhenAny(
+                imuOn.Task,
+                sampleBeforeOn.Task,
+                Task.Delay(TimeSpan.FromSeconds(3))
+            );
+            if (onOrFailure == sampleBeforeOn.Task)
+            {
+                Assert.Fail("Received IMU sample before ON state notification");
+            }
+            else if (onOrFailure != imuOn.Task)
+            {
+                Assert.Fail("Timed out waiting for IMU ON notification");
+            }
+
+            var sampleResult = await Task.WhenAny(
+                imuSample.Task,
+                sampleBeforeOn.Task,
+                Task.Delay(TimeSpan.FromSeconds(3))
+            );
+            if (sampleResult == sampleBeforeOn.Task)
+            {
+                Assert.Fail("Received IMU sample before ON state notification");
+            }
+            else if (sampleResult != imuSample.Task)
+            {
+                Assert.Fail("Timed out waiting for IMU sample after ON notification");
+            }
+
+            lock (stateNotifications)
+            {
+                Assert.IsTrue(
+                    stateNotifications.Count > 0,
+                    "Expected at least one IMU state notification"
+                );
+            }
         }
         finally
         {
